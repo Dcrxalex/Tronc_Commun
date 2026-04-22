1bff00bbf7a68f838046b0a5c0ce58250ee8d011
Dcrxalex <122109696+Dcrxalex@users.noreply.github.com>
2026-04-22 17:08:37 +0200

    Add files via upload

--- Tronc_commun/M0/libft/ft_calloc.c
+++ Tronc_commun/M0/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aldecroi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:02:03 by aldecroi          #+#    #+#             */
-/*   Updated: 2026/04/07 11:03:07 by aldecroi         ###   ########.fr       */
+/*   Updated: 2026/04/21 10:45:05 by decro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*r;
 
+	if (size == 0 || count == 0)
+	{
+		r = malloc(1);
+		return (r);
+	}
 	r = malloc(count * size);
 	if (!r)
 		return (NULL);
